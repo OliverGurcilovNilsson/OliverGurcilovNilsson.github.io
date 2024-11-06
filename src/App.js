471c62b6efbfc69afc706b7245a1d133fd6f096a
oliver <oliver.gurcilov1nilsson@gmail.com>
2024-11-06 16:56:59 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Education from './components/Education';
 
 function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <h1>Oliver Gurcilov Nilsson</h1>
-          </header>
-      </div>
-  );
+    const [page, setPage] = useState('home');
+
+    const renderPage = () => {
+        switch (page) {
+            case 'education':
+                return <Education />;
+            case 'home':
+            default:
+                return (
+                    <div className="home">
+                        <h1>Welcome to My Portfolio</h1>
+                        <p>Explore my background and work.</p>
+                    </div>
+                );
+        }
+    };
+
+    return (
+        <div className="App">
+            <Navbar onNavigate={setPage} />
+            <header className="App-header">
+                {renderPage()}
+            </header>
+        </div>
+    );
 }
 
 export default App;
