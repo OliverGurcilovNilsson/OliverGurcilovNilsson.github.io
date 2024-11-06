9ed3b57aac85a43d7d5bb08c641e1f67b7e970d3
oliver <oliver.gurcilov1nilsson@gmail.com>
2024-11-06 16:39:42 +0100

    removed education

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import {educationList} from "./components/Education";
-import {EducationItem} from "./components/Education";
 
 function App() {
   return (
       <div className="App">
           <header className="App-header">
               <h1>Oliver Gurcilov Nilsson</h1>
-              <h2>Education</h2>
-              {educationList.map((education: EducationItem, index: number) => (
-                  <div key={index} style={{ marginBottom: '20px' }}>
-                      <h3>{education.degree} in {education.fieldOfStudy}</h3>
-                      <h4>{education.institution}</h4>
-                      <p>{education.startDate} - {education.endDate}</p>
-                      {education.description && <p>{education.description}</p>}
-                  </div>
-              ))}
           </header>
       </div>
   );
