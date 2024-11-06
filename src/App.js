5998602e22fa3dc34010dfdda526d6bdfe3a9912
oliver <oliver.gurcilov1nilsson@gmail.com>
2024-11-06 15:13:48 +0100

    Remove node.js.yml from tracking

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi This is a change made! <code>src/App.js</code> and save to reload.
+          Oliver Gurcilov Nilsson
         </p>
         <a
           className="App-link"
