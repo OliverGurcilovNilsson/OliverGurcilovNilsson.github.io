7dbe79f7eecebebae9dc60f65f4eb50515fde59a
oliver <oliver.gurcilov1nilsson@gmail.com>
2024-11-06 23:18:12 +0100

    small changes to UI in homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Education from './components/Education'; // Correctly import Education
+import Education from './components/Education';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faDribbble } from '@fortawesome/free-brands-svg-icons';
+import { faSmile } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
     const [currentPage, setCurrentPage] = useState('home');
@@ -10,10 +14,30 @@ function App() {
 
     return (
         <div className="App">
-            <Navbar onNavigate={handleNavigate} />
+            <Navbar onNavigate={handleNavigate}/>
             <header className="App-header">
-                {currentPage === 'home' && <h1>Welcome to My Portfolio</h1>}
-                {currentPage === 'education' && <Education />}
+                <div className="content-box">
+                    {currentPage === 'home' && <h1>Welcome</h1>}
+                    {currentPage === 'education' && <Education/>}
+
+                    {currentPage === 'home' && (
+                        <div className="social-icons">
+                            <button className="social-btn">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </button>
+                            <button className="social-btn">
+                                <FontAwesomeIcon icon={faLinkedin} />
+                            </button>
+                            <button className="social-btn">
+                                <FontAwesomeIcon icon={faDribbble} />
+                            </button>
+                            <button className="social-btn">
+                                <FontAwesomeIcon icon={faSmile} />
+                            </button>
+                            <button className="social-btn">Book a call</button>
+                        </div>
+                    )}
+                </div>
             </header>
         </div>
     );
